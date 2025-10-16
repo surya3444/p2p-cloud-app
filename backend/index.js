03687552d432317efd0a3054ba69594b4693fb80
surya3444 <isuryakarthikvarma@gmail.com>
2025-10-16 16:20:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const { Server } = require('socket.io');
 
 // --- APP & SERVER SETUP ---
 const app = express();
-const whitelist = ['http://localhost:5173', 'http://localhost:4173', 'https://your-project-name.vercel.app'];
+const whitelist = ['http://localhost:5173', 'http://localhost:4173', 'https://p2p-cloud-server.vercel.app'];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
