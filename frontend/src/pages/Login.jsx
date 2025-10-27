31e6cad4091c23a84d44ded7d6fd429e49cc4565
surya3444 <isuryakarthikvarma@gmail.com>
2025-10-27 21:40:51 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
         onSuccess: async (codeResponse) => {
             setMessage('Authenticating with Google...');
             try {
-                const response = await fetch('https://p2pcloudapp-a5k7yqu2.b4a.run/api/auth/google', {
+                const response = await fetch('https://p2p-backend-7ex8.onrender.com//api/auth/google', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ code: codeResponse.code }),
@@ -38,7 +38,7 @@ function Login() {
         e.preventDefault();
         setMessage('Logging in...');
         try {
-            const response = await fetch('https://p2p-backend-production-4e46.up.railway.app/api/login', {
+            const response = await fetch('https://p2p-backend-7ex8.onrender.com/api/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(formData),
