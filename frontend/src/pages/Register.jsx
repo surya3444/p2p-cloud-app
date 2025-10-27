64388b6008fd7fb373d788e46513a58a530d214b
surya3444 <isuryakarthikvarma@gmail.com>
2025-10-27 19:52:49 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -48,7 +48,7 @@ function Register() {
 
         setMessage('Registering...');
         try {
-            const response = await fetch('https://p2p-backend-production-4e46.up.railway.app/api/register', {
+            const response = await fetch('https://p2pcloudapp-t33yuvp3.b4a.run/api/register', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(formData),
