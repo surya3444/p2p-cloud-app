f4a4cbf96af68ced9425ed1efe7578e32dd53977
surya3444 <isuryakarthikvarma@gmail.com>
2025-10-27 22:10:59 +0530

    Update Verify.jsx

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -21,7 +21,7 @@ function Verify() {
         }
 
         try {
-            const response = await fetch('https://p2pcloudapp-t33yuvp3.b4a.run/api/verify', {
+            const response = await fetch('https://p2p-backend-7ex8.onrender.com/api/verify', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, otp }),
